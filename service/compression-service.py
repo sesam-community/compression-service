cf9eb1b6179911ade70e9ef809c8fd3306fc0172
Anders Borud <post@andersborud.com>
2020-11-20 14:50:35 +0100

    Restructure and start using the sesamutils package

--- service/compression-service.py
+++ service/compression-service.py
@@ -1,64 +1,31 @@
-from flask import Flask, request, Response
-import os
-import cherrypy
-import json
-import logging
-import paste.translogger
+from flask import Flask, request, Response, abort
 import requests
 import zlib
-
-
+from sesamutils import sesam_logger
+from sesamutils.flask import serve
 
 app = Flask(__name__)
+logger = sesam_logger("compression-service", app=app)
 
-logger = logging.getLogger("compression-service")
 
-@app.route('/', methods=['GET'])
-def root():
-    return Response(status=200, response="Working.")
-
-@app.route('/gzip', methods=['GET'])
-def get_gzip():
-    full_url = request.args.get('url', None) # use default value repalce 'None'
-
-    def deco_file(url):
-      d = zlib.decompressobj(16+zlib.MAX_WBITS)
-      with requests.get(url, stream=True) as r:
-
+def decode_file(url):
+    d = zlib.decompressobj(16 + zlib.MAX_WBITS)
+    with requests.get(url, stream=True) as r:
         for chunk in r.iter_content(chunk_size=1048576, decode_unicode=False):
             if chunk:
                 yield d.decompress(chunk)
 
-    return Response(deco_file(full_url), mimetype='application/json', direct_passthrough=True)
+
+@app.route('/gzip', methods=['GET'])
+def get_gzip():
+    full_url = request.args.get('url', None)  # use default value replace 'None'
+    logger.info(f"processing request for {full_url}")
+    if not full_url:
+        return abort(400, "Missing url param in request.")
+
+    return Response(decode_file(full_url), mimetype='application/json', direct_passthrough=True)
 
 
-if __name__ == '__main__':
-    format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-
-    # Log to stdout, change to or add a (Rotating)FileHandler to log to a file
-    stdout_handler = logging.StreamHandler()
-    stdout_handler.setFormatter(logging.Formatter(format_string))
-    logger.addHandler(stdout_handler)
-
-    # Comment these two lines if you don't want access request logging
-    app.wsgi_app = paste.translogger.TransLogger(app.wsgi_app, logger_name=logger.name,
-                                                 setup_console_handler=False)
-    app.logger.addHandler(stdout_handler)
-
-    logger.propagate = False
-    logger.setLevel(logging.INFO)
-
-    cherrypy.tree.graft(app, '/')
-
-    # Set the configuration of the web server to production mode
-    cherrypy.config.update({
-        'environment': 'production',
-        'engine.autoreload_on': False,
-        'log.screen': True,
-        'server.socket_port': 5001,
-        'server.socket_host': '0.0.0.0'
-    })
-
-    # Start the CherryPy WSGI web server
-    cherrypy.engine.start()
-    cherrypy.engine.block()
+if __name__ == "__main__":
+    logger.info("Starting service...")
+    serve(app)
